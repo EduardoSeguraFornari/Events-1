780b9a5342db3c5f38afb470e6a0729b2710fbd7
Homero Oliveira <homero.oliveira@ilegra.com>
2019-04-09 11:25:15 -0300

    add swiftlint.yml

--- Events/AppDelegate.swift
+++ Events/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Events
-//
-//  Created by ilegra-mac000551 on 09/04/19.
-//  Copyright © 2019 Homero Oliveira. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
@@ -13,11 +5,10 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
-    
-}
 
+}
