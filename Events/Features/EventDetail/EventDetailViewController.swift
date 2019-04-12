b0448a47bb7f38f63c603269f87f70658abec40e
Homero Oliveira <homero.oliveira@ilegra.com>
2019-04-12 04:20:14 -0300

    created viewMoel and viewController

---
+++ Events/Features/EventDetail/EventDetailViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+final class EventDetailViewController: UIViewController {
+
+    let viewModel: EventDetailViewModel
+    
+    init(viewModel: EventDetailViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        return nil
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+//MARK: - UITableViewDataSource
+extension EventDetailViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
